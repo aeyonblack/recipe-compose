af7af057d922e524f003967767ade57217e6bc16
aeyonblack <tanyamasvitaog@gmail.com>
2021-09-22 19:05:46 +0200

    recipe detail screen

--- app/src/main/java/com/tanya/recipecompose/presentation/components/RecipeView.kt
+++ app/src/main/java/com/tanya/recipecompose/presentation/components/RecipeView.kt
@@ -1,13 +1,21 @@
 package com.tanya.recipecompose.presentation.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import com.tanya.recipecompose.domain.model.Recipe
+import com.tanya.recipecompose.util.DEFAULT_RECIPE_IMAGE
+import com.tanya.recipecompose.util.IMAGE_HEIGHT
+import com.tanya.recipecompose.util.loadPicture
 
 @Composable
 fun RecipeView(
@@ -18,6 +26,69 @@ fun RecipeView(
             .fillMaxWidth()
             .verticalScroll(rememberScrollState())
     ) {
-
+        recipe.featuredImage?.let {
+            val image = loadPicture(
+                url = it,
+                defaultImage = DEFAULT_RECIPE_IMAGE
+            ).value
+            image?.let { img ->
+                Image(
+                    bitmap = img.asImageBitmap(),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .requiredHeight(IMAGE_HEIGHT.dp),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = "recipe featured image"
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
+            recipe.title?.let {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp)
+                ) {
+                    Text(
+                        text = it,
+                        modifier = Modifier
+                            .fillMaxWidth(0.85f)
+                            .wrapContentWidth(Alignment.Start),
+                        style = MaterialTheme.typography.h4
+                    )
+                    val rank = recipe.rating.toString()
+                    Text(
+                        text = rank,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.End)
+                            .align(Alignment.CenterVertically),
+                        style = MaterialTheme.typography.h5
+                    )
+                }
+            }
+            recipe.publisher?.let {
+                val updated = recipe.dateUpdated
+                Text(
+                    text = if (updated != null) "Updated $updated by $it" else "By $it",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    style = MaterialTheme.typography.caption
+                )
+            }
+            for (ingredient in recipe.ingredients) {
+                Text(
+                    text = ingredient,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 8.dp),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/tanya/recipecompose/presentation/ui/recipe/RecipeFragment.kt
+++ app/src/main/java/com/tanya/recipecompose/presentation/ui/recipe/RecipeFragment.kt
@@ -4,9 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
@@ -14,21 +18,29 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.tanya.recipecompose.presentation.BaseApplication
+import com.tanya.recipecompose.presentation.components.CircularIndeterminateProgressBar
+import com.tanya.recipecompose.presentation.components.RecipeView
 import com.tanya.recipecompose.presentation.ui.recipe.RecipeEvent.*
 import com.tanya.recipecompose.ui.theme.RecipeComposeTheme
+import com.tanya.recipecompose.util.SnackbarController
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class RecipeFragment : Fragment() {
 
+    @Inject lateinit var app: BaseApplication
+
     private val viewModel:RecipeViewModel by viewModels()
 
-    private var recipeId: MutableState<Int?> = mutableStateOf(null)
+    private val snackbarController = SnackbarController(lifecycleScope)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,13 +61,23 @@ class RecipeFragment : Fragment() {
             setContent {
                 val recipe = viewModel.recipe.value
                 val loading = viewModel.loading.value
+                val scaffoldState = rememberScaffoldState()
 
-                RecipeComposeTheme {
-                    Column(modifier = Modifier.padding(all=16.dp)) {
-                        Text(
-                            text =
-                            recipe?.let { "${it.title}" }?:"loading..."
-                        )
+                RecipeComposeTheme(darkTheme = app.isDark.value) {
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        snackbarHost = { scaffoldState.snackbarHostState }
+                    ) {
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            if (loading && recipe == null)
+                                Text(text = "Loading...")
+                            else {
+                                recipe?.let {
+                                    RecipeView(recipe = it)
+                                }
+                            }
+                            CircularIndeterminateProgressBar(displayed = loading)
+                        }
                     }
                 }
             }
